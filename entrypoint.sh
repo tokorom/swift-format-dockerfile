82672e1f54260343c8a8203ff0a958b84ec5dd1f
tokorom <tokorom@gmail.com>
2020-11-07 08:22:20 +0900

    Improve entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ -n "$SWIFT_FORMAT_ARGUMENTS" ]; then
   if [ -n "$SWIFT_FORMAT_OUTPUTFILE" ]; then
-    swift-format $(echo $SWIFT_FORMAT_ARGUMENTS) &>$SWIFT_FORMAT_OUTPUTFILE
+    swift-format $(echo $SWIFT_FORMAT_ARGUMENTS) 2>$SWIFT_FORMAT_OUTPUTFILE
   else
     swift-format $(echo $SWIFT_FORMAT_ARGUMENTS)
   fi
